afae79b4e4bb89623107ab7dbf0caf1fed1cad0d
Michelle <qianranma8@gmail.com>
2025-10-25 16:09:00 -0400

    add Card class

---
+++ src/Card.java
@@ -0,0 +1,29 @@
+public class Card {
+    private String rank;
+    private String suit;
+
+    public Card(String rank, String suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public int getValue(){
+        if (this.rank.equals("A")){
+            return 11;
+        } else if (this.rank.equals("J") || this.rank.equals("Q") || this.rank.equals("K") || this.rank.length() == 2){
+            return 10;
+        } else {
+            return this.rank.charAt(0) - '0';
+        }
+    }
+
+    public String toString(){
+        return this.rank + this.suit;
+    }
+
+    public static void main(String[] args) {
+        Card card = new Card("10", "H");
+        System.out.println("The card value is " + card.getValue());
+        System.out.println("The card is "+ card.toString());
+    }
+}
